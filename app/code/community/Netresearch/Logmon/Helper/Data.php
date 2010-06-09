8807bc3b39bda84fa3918e64b4ee1da6fdf03f91
Thomas Kappel <thomas.kappel@netresearch.de>
2010-06-09 15:32:17 +0200

    bur-970 - prepared config area

---
+++ app/code/community/Netresearch/Logmon/Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @category   Logging_Monitoring
+ * @package    Netresearch_Logmon
+ * @author     Thomas Kappel <thomas.kappel@netresearch.de>
+ * @copyright  Copyright (c) 2010 Netresearch GmbH & Co.KG <http://www.netresearch.de/>
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Netresearch_Logmon_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    
+}
